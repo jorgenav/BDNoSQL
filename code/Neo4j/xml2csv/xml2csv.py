84c3e3734b14ab48d3b15197ad1f4f8b82a38f3b
jorgenav <jornaga1@gmail.com>
2017-05-06 12:52:33 +0200

    Neo4j to csv script cleaned

--- code/Neo4j/xml2csv/xml2csv.py
+++ code/Neo4j/xml2csv/xml2csv.py
@@ -72,17 +72,15 @@ parser.entity["Ucirc"] = 'Û'
 parser.entity["Euml"] = 'Ë'
 
 
-xml_file = '../../../data/dblp.0.xml'
-# xml_file = '../../../data/dblp.xml'
+xml_file = '../../../data/dblp.xml'
 
 e = ET.parse(xml_file, parser=parser).getroot()
 
-# html.unescape(f.read()).replace('&','&#038;')
 
-d = {}
 docs = ['article', 'inproceedings', 'incollection']
 tags = ['author', 'year', 'title']
 
+# Diccionarios de nodos
 dict_autores = OrderedDict()
 dict_autores["authorId:ID(Authors)"] = []
 dict_autores["name"] = []
@@ -95,15 +93,16 @@ dict_publicaciones["Year"] = []
 dict_publicaciones["Tipo"] = []
 dict_publicaciones[":LABEL"] = []
 
+dict_autores_general = {}
+dict_pub_general = {}
+
+# Diccionarios de relaciones
 dict_rel_AA = OrderedDict()
 dict_rel_AA[":START_ID(Authors)"] = []
 dict_rel_AA["Title"] = []
 dict_rel_AA["Year"] = []
 dict_rel_AA[":END_ID(Authors)"] = []
 dict_rel_AA[":TYPE"] = []
-
-dict_autores_general = {}
-dict_pub_general = {}
 
 dict_rel_AP = OrderedDict()
 dict_rel_AP[":START_ID(Authors)"] = []
@@ -119,12 +118,8 @@ pubId = 1
 for child1 in e:
     list_rel_AA = []
     list_rel_AP = []
-    # print("Next doc---------------------------------------------")
     if (child1.tag in docs):
 
-        # d[child1.tag] = OrderedDict()
-        # d[child1.tag]['doc_type'] = child1.tag
-        # d[child1.tag]['author'] = []
         dict_publicaciones[":LABEL"].append("Pub")
         dict_publicaciones["Tipo"].append(child1.tag)
 
@@ -134,7 +129,6 @@ for child1 in e:
                     dict_publicaciones["pubId:ID(Pubs)"].append(pubId)
                     dict_publicaciones["Title"].append(child2.text)
                     title_rel_AA = child2.text
-                    # dict_rel_AP[":END_ID(Pubs)"].append(dict_pub_general[child2.text])
                     dict_pub_general[child2.text] = pubId
                     pubId += 1
                 elif (child2.tag == 'year'):
@@ -144,41 +138,20 @@ for child1 in e:
                     if child2.text not in dict_autores["name"]:
                         dict_autores["authorId:ID(Authors)"].append(authorId)
                         dict_autores["name"].append(child2.text)
-
                         dict_autores_general[child2.text] = authorId
-
                         dict_autores[":LABEL"].append("Autor")
                         authorId += 1
                     if child2.text not in list_rel_AA:
                         list_rel_AA.append(dict_autores_general[child2.text])
-
-
-                        # list_rel_AA.append(dict_autores["name"])
-
-                        # dict_autores["name"] == child2.text
-                        # dict_autores["authorId:ID(Author)"]
-
-
-                    # d[child1.tag]['author'].append(child2.text)
-                else:
-                    # d[child1.tag][child2.tag] = child2.text
-                    pass
         cont_rel_AA = 0
         for i in range(len(list_rel_AA)):
-            # print(len(list_rel_AA))
             dict_rel_AP[":START_ID(Authors)"].append(list_rel_AA[i])
             dict_rel_AP[":TYPE"].append("Autoria")
             dict_rel_AP[":END_ID(Pubs)"].append(dict_pub_general[title_rel_AA])
-            # print(dict_rel_AP[":START_ID(Authors)"])
-            # print(dict_rel_AP[":TYPE"])
-            # print(dict_rel_AP[":END_ID(Pubs)"])
 
-            for j in range(i+1,len(list_rel_AA)):
+            for j in range(i+1, len(list_rel_AA)):
                 dict_rel_AA[":START_ID(Authors)"].append(list_rel_AA[i])
                 dict_rel_AA[":END_ID(Authors)"].append(list_rel_AA[j])
-                # print(cont_rel_AA)
-                # print(list_rel_AA[i])
-                # print(list_rel_AA[j])
                 cont_rel_AA += 1
 
         dict_rel_AA[":TYPE"] = np.repeat("Collaborate", len(dict_rel_AA[":START_ID(Authors)"]))
@@ -195,30 +168,7 @@ publicaciones = pd.DataFrame(dict_publicaciones)
 relation_AA = pd.DataFrame(dict_rel_AA)
 relation_AP = pd.DataFrame(dict_rel_AP)
 
-autores.to_csv("results/autores_T.csv", index = False)
-publicaciones.to_csv("results/publicaciones_T.csv", index = False)
-relation_AA.to_csv("results/relation_AA.csv_T", index = False)
-relation_AP.to_csv("results/relation_AP.csv_T", index = False)
-
-# with open("results/autores.csv", "w") as out:
-#     out.write(autores)
-#
-# with open("results/publicaciones.csv", "w") as out:
-#     out.write(publicaciones)
-#
-# with open("results/relation_AA.csv", "w") as out:
-#     out.write(relation_AA)
-#
-# with open("results/relation_AP.csv", "w") as out:
-#     out.write(relation_AP)
-
-
-# out.writelines(json.dumps(d[child1.tag])+'\n')
-
-
-
-# print(autores)
-# print(publicaciones)
-# print(relation_AA)
-# print(relation_AP)
-
+autores.to_csv("results/autores_T.csv", index=False)
+publicaciones.to_csv("results/publicaciones_T.csv", index=False)
+relation_AA.to_csv("results/relation_AA.csv_T", index=False)
+relation_AP.to_csv("results/relation_AP.csv_T", index=False)
